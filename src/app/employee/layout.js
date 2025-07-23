5c167d5cc30cdffd5201c38a7f9fa700fafc96ac
Neo Phetaone <phetoaneneo4@gmail.com>
2025-07-23 13:39:48 +0200

    Add sidebar and logo components; update dependencies and remove unused SVGs.

--- src/app/employee/layout.js
+++ src/app/employee/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Sidebar from "./components/Sidebar";
 
 export const metadata = {
   title: "Employee Dashboard (Managemant System)",
@@ -8,10 +9,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={` antialiased`}
-      >
-        {children}
+      <body className={` antialiased`}>
+        <div className="flex min-h-screen">
+          {/* Sidebar */}
+          <Sidebar />
+          {children}
+        </div>
       </body>
     </html>
   );
