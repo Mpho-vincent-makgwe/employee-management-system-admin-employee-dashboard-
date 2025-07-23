58adf3e61cddf7c54038d43be0bc810ce2a27091
Neo Phetaone <phetoaneneo4@gmail.com>
2025-07-23 17:30:06 +0200

    Refactor MyTimesheet page structure; create MyTimesheetPage component and update layout with SearchProvider.

--- src/app/employee/layout.js
+++ src/app/employee/layout.js
@@ -1,21 +1,24 @@
-import "./globals.css";
+import "../globals.css";
 import Sidebar from "./components/Sidebar";
+import { SearchProvider } from "./context/SearchContext";
 
 export const metadata = {
   title: "Employee Dashboard (Managemant System)",
   description: "Employee dashboard for employee managent system",
 };
 
-export default function RootLayout({ children }) {
+export default function EmployeeLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={` antialiased`}>
-        <div className="flex min-h-screen">
-          {/* Sidebar */}
-          <Sidebar />
+    <SearchProvider>
+      <div className="flex min-h-screen">
+        {/* Sidebar */}
+        <Sidebar />
+
+        {/* Main Content */}
+        <main className="flex-1 flex flex-col overflow-hidden pt-16 lg:pl-64 bg-gray-100">
           {children}
-        </div>
-      </body>
-    </html>
+        </main>
+      </div>
+    </SearchProvider>
   );
 }
--- src/app/employee/my-timesheet/page.jsx
+++ src/app/employee/my-timesheet/page.jsx
@@ -1,16 +1,9 @@
-import TimesheetSummaryCards from "../components/SummaryCards";
-import Heading from "../components/Heading";
-import TimesheetEntries from "../components/TimesheetEntries";
+import MyTimesheetPage from "../components/MyTimesheetPage";
 
 export default function MyTimesheet() {
   return (
-    <main>
-      <Heading
-        title="My Timesheet"
-        subtitle="Track your working hours and time entries."
-      />
-      <TimesheetSummaryCards />
-      <TimesheetEntries />
-    </main>
+    <>
+      <MyTimesheetPage />
+    </>
   );
 }
