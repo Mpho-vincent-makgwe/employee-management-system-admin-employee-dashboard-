9e937b878ef9d93b1ad52427f9e6504dc38f2fe8
Mosobe Lulu <mosobelulu@gmail.com>
2025-08-04 10:54:43 +0200

    timesheet tracking

--- src/app/admin/timesheet/page.jsx
+++ src/app/admin/timesheet/page.jsx
@@ -1,8 +1,19 @@
 import TimesheetCards from "@/components/AdminComponents/TimesheetCards"
+import TimesheetTracking from "@/components/AdminComponents/TimeSheetTracking"
 
 export default function Timesheet() {
     return(
+        <div>
+            
+      <div className="mb-6">
+        <h1 className="text-2xl font-bold text-gray-800">Timesheet Tracking</h1>
+        <p className="text-sm text-gray-600">
+          Attendance and working hours on the 4th of July, 2025
+        </p>
+      </div>
         <TimesheetCards/>
-        
+        <TimesheetTracking />
+        </div>
+
     )
 }
