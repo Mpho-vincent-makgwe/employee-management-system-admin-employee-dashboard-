6218b01defea86825c253f6104f5dbf54d937997
Mosobe Lulu <mosobelulu@gmail.com>
2025-08-04 10:32:02 +0200

    added timesheet overview cards

--- src/app/admin/timesheet/page.jsx
+++ src/app/admin/timesheet/page.jsx
@@ -1,42 +1,8 @@
-// "use client";
+import TimesheetCards from "@/components/AdminComponents/TimesheetCards"
 
-// import Table from "@/components/Table";
-// import { timesheetData } from "@/data/adminData/timesheetData";
-
-// const columns = [
-//   { key: "date", title: "Date" },
-//   { key: "status", title: "Status" },
-//   { 
-//     key: "clockIn", 
-//     title: "Clock In",
-//     render: (row) => <div className="text-center">{row.clockIn}</div>
-//   },
-//   { 
-//     key: "clockOut", 
-//     title: "Clock Out",
-//     render: (row) => <div className="text-center">{row.clockOut}</div>
-//   }
-// ];
-
-// const statusColorMap = {
-//   Present: "text-green-500",
-//   Late: "text-yellow-500",
-//   Absent: "text-red-500"
-// };
-
-// export default function MyTimesheet() {
-//   return (
-//     <div className="p-4">
-//       <Table
-//         title="My Timesheet"
-//         subtitle="Your latest attendance log"
-//         columns={columns}
-//         data={timesheetData}
-//         statusColorMap={statusColorMap}
-//         limit={5}
-//         enablePagination={true}
-//         stripedRows={true}
-//       />
-//     </div>
-//   );
-// }
+export default function Timesheet() {
+    return(
+        <TimesheetCards/>
+        
+    )
+}
