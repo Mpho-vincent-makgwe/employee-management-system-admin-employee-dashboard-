53b08ab782124b57538b4988df57b973b5d7c6e4
Neo Phetaone <phetoaneneo4@gmail.com>
2025-08-06 21:29:25 +0200

    Added BarChart component to Employee Dashboard.

--- src/app/employee/dashboard/page.jsx
+++ src/app/employee/dashboard/page.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import RecentEntries from "@/components/EmployeeComponents/RecentEntries";
+import BarChart from "@/components/EmployeeComponents/BarChart";
 
 const page = () => {
   return (
     <div>
+      <BarChart />
       <RecentEntries />
     </div>
   );
