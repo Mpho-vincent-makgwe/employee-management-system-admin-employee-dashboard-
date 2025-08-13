0308da8d700d876bbef8afa2222b682e0435189f
Neo Phetoane <phetoaneneo4@gmail.com>
2025-08-13 22:04:44 +0200

    Refactor BarChart component and add Card component for better structure.

--- src/components/EmployeeComponents/BarChart.jsx
+++ src/components/EmployeeComponents/BarChart.jsx
@@ -10,7 +10,7 @@ import {
   Tooltip,
   Legend,
 } from "chart.js";
-import Card from "../AdminComponents/Card";
+import Card from "./Card";
 import { Poppins, Montserrat } from "next/font/google";
 
 ChartJS.register(
@@ -45,12 +45,13 @@ const BarChart = () => {
     datasets: [
       {
         label: "Working Hours",
+        height: "22px",
         data: [1, 2, 3.5, 4.1, 7],
         backgroundColor: "#6366F1",
-        borderRadius: 3,
-        barThickness: 40,
-        categoryPercentage: 0.6,
-        barPercentage: 0.9,
+        // borderRadius: 3,
+        barThickness: 12,
+        // categoryPercentage: 0.6,
+        // barPercentage: 0.9,
       },
     ],
   };
