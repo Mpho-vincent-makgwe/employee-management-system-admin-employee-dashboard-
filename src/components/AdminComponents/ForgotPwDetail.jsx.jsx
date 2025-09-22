016d2cbdaad9f8ebbc4de3c10a574d1783f5b38f
Mosobe Lulu <mosobelulu@gmail.com>
2025-09-22 08:33:19 +0200

    moved notification Modal to ui

--- src/components/AdminComponents/ForgotPwDetail.jsx.jsx
+++ src/components/AdminComponents/ForgotPwDetail.jsx.jsx
@@ -1,122 +1,125 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import NotificationModal from './NotificationModal';
-import { CheckCircleIcon } from 'lucide-react';
-import { FiEye, FiEyeOff } from 'react-icons/fi';
-import { FaCheck } from 'react-icons/fa';
+import { useState } from "react";
+import NotificationModal from "../../ui/NotificationModal";
+import { CheckCircleIcon } from "lucide-react";
+import { FiEye, FiEyeOff } from "react-icons/fi";
+import { FaCheck } from "react-icons/fa";
 
 export default function ForgotPwDetail() {
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [showSuccess, setShowSuccess] = useState(false)
-  const [showPassword, setShowPassword] = useState(false)
-  const [showConfirm, setShowConfirm] = useState(false)
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [showSuccess, setShowSuccess] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirm, setShowConfirm] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Password reset request:', { password, confirmPassword });
+    console.log("Password reset request:", { password, confirmPassword });
     setShowSuccess(true);
   };
 
-    const toggleShowPassword = () => setShowPassword((prev) => !prev);
+  const toggleShowPassword = () => setShowPassword((prev) => !prev);
   const toggleShowConfirm = () => setShowConfirm((prev) => !prev);
 
   return (
-    <div >
-  <form className="w-full bg-white rounded-lg shadow-sm h-auto p-6 space-y-6">
-    <div className='space-y-2'>
-  <h2 className="text-lg font-semibold text-[#4F46E5] ">Reset Password</h2>
-      <p className='text-black'>Enter New Password</p>
-    </div>
-    
-
-      <div className="grid grid-cols-1 md:grid-cols-1 gap-6 items-start">
-       
-        <div className="w-1/2 relative">
-          <label className="block text-sm font-medium text-gray-700 mb-2">New Password</label>
-          <input
-            type={showPassword ? 'text' : 'password'}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Enter new password"
-            className="w-full h-[50px] px-4 py-2 pr-10 border border-gray-300 rounded-md text-sm focus:outline-none focus:ring-1 focus:ring-indigo-500"
-            required
-          />
-          <button
-            type="button"
-            onClick={toggleShowPassword}
-            className="absolute top-[34px] right-3 text-gray-500"
-          >
-            {showPassword ? <FiEyeOff className="h-5 w-5" /> : <FiEye className="h-5 w-5" />}
-          </button>
+    <div>
+      <form className="w-full bg-white rounded-lg shadow-sm h-auto p-6 space-y-6">
+        <div className="space-y-2">
+          <h2 className="text-lg font-semibold text-[#4F46E5] ">
+            Reset Password
+          </h2>
+          <p className="text-black">Enter New Password</p>
         </div>
 
-     
-        <div className="w-1/2 relative">
-          <label className="block text-sm font-medium text-gray-700 mb-2">Confirm Password</label>
-          <input
-            type={showConfirm ? 'text' : 'password'}
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            placeholder="Re-enter password"
-            className="w-full h-[50px] px-4 py-2 pr-10 border border-gray-300 rounded-md text-sm focus:outline-none focus:ring-1 focus:ring-indigo-500"
-            required
-          />
-          <button
-            type="button"
-            onClick={toggleShowConfirm}
-            className="absolute top-[34px] right-3 text-gray-500"
-          >
-            {showConfirm ? <FiEyeOff className="h-5 w-5" /> : <FiEye className="h-5 w-5" />}
-          </button>
+        <div className="grid grid-cols-1 md:grid-cols-1 gap-6 items-start">
+          <div className="w-1/2 relative">
+            <label className="block text-sm font-medium text-gray-700 mb-2">
+              New Password
+            </label>
+            <input
+              type={showPassword ? "text" : "password"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter new password"
+              className="w-full h-[50px] px-4 py-2 pr-10 border border-gray-300 rounded-md text-sm focus:outline-none focus:ring-1 focus:ring-indigo-500"
+              required
+            />
+            <button
+              type="button"
+              onClick={toggleShowPassword}
+              className="absolute top-[34px] right-3 text-gray-500"
+            >
+              {showPassword ? (
+                <FiEyeOff className="h-5 w-5" />
+              ) : (
+                <FiEye className="h-5 w-5" />
+              )}
+            </button>
+          </div>
+
+          <div className="w-1/2 relative">
+            <label className="block text-sm font-medium text-gray-700 mb-2">
+              Confirm Password
+            </label>
+            <input
+              type={showConfirm ? "text" : "password"}
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              placeholder="Re-enter password"
+              className="w-full h-[50px] px-4 py-2 pr-10 border border-gray-300 rounded-md text-sm focus:outline-none focus:ring-1 focus:ring-indigo-500"
+              required
+            />
+            <button
+              type="button"
+              onClick={toggleShowConfirm}
+              className="absolute top-[34px] right-3 text-gray-500"
+            >
+              {showConfirm ? (
+                <FiEyeOff className="h-5 w-5" />
+              ) : (
+                <FiEye className="h-5 w-5" />
+              )}
+            </button>
+          </div>
         </div>
-      </div>
 
+        <ul className="text-sm text-gray-700 list-none space-y-2 mt-4">
+          {[
+            "Have at least 8 characters",
+            "Have at least one number",
+            "Have at least an uppercase",
+            "Have a special character (#$@!%&*-)",
+          ].map((rule, idx) => (
+            <li key={idx} className="flex items-center gap-3">
+              <span className="w-5 h-5 rounded-[4.4px] bg-[#4F46E5] flex items-center justify-center text-white text-xs">
+                <FaCheck />
+              </span>
+              {rule}
+            </li>
+          ))}
+        </ul>
 
-        
-      
-
-<ul className="text-sm text-gray-700 list-none space-y-2 mt-4">
-  {[
-    'Have at least 8 characters',
-    'Have at least one number',
-    'Have at least an uppercase',
-    'Have a special character (#$@!%&*-)',
-  ].map((rule, idx) => (
-    <li key={idx} className="flex items-center gap-3">
-      <span className="w-5 h-5 rounded-[4.4px] bg-[#4F46E5] flex items-center justify-center text-white text-xs">
-        <FaCheck />
-      </span>
-      {rule}
-    </li>
-  ))}
-</ul>
-
-
-       
         <div className="pt-6">
           <button
-          onClick={handleSubmit}
+            onClick={handleSubmit}
             type="submit"
             className=" w-1/2 px-6 py-3 bg-[#4F46E5] text-white rounded-[4px] font-medium text-sm hover:bg-[#4338CA]"
-
           >
             Reset Password
           </button>
         </div>
       </form>
 
-       <NotificationModal
+      <NotificationModal
         show={showSuccess}
         close={() => setShowSuccess(false)}
         icon={<CheckCircleIcon className="w-6 h-6 text-white" />}
-        title= 'Successful'
+        title="Successful"
         message="Your password has been reset.  
 You can now use your new password to sign in."
-        buttonLabel='Go Back'
+        buttonLabel="Go Back"
         onConfirm={() => setShowSuccess(false)}
-       
       />
     </div>
   );
