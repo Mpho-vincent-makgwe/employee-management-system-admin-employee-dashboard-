79fba596da0c26d607bcedbe53e9a8cfe1e9404e
Mosobe Lulu <mosobelulu@gmail.com>
2025-09-24 20:39:03 +0200

    added a dynamic card

--- src/components/EmployeeComponents/my-attendance/AttendanceSummaryCards.jsx
+++ src/components/EmployeeComponents/my-attendance/AttendanceSummaryCards.jsx
@@ -1,9 +1,9 @@
 import { timesheetEntries } from "../../../data/employeeData/timesheetData";
 import { getTimeSummary } from "@/utils/attendanceUtils";
 import { getAttendanceSummary } from "@/utils/attendanceUtils";
-
-import { Clock } from "lucide-react";
 import { BsClockFill } from "react-icons/bs";
+import Card from "@/components/ui/Card";
+
 const AttendanceSummary = () => {
   const summary = getAttendanceSummary(timesheetEntries);
 
@@ -17,12 +17,12 @@ const AttendanceSummary = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-8">
       {items.map(({ title, value }) => (
-        <div
+        <Card
           key={title}
           className="w-full h-[124px] p-4 border-[0.4px] border-[#D0D5DD] rounded-[8px] bg-white flex flex-col sm:flex-row items-center gap-[10px]"
         >
           {/* Icon */}
-          <div className="w-[40px] h-[40px] bg-[#4F46E5] rounded-full flex items-center justify-center">
+          <div className="w-[48px] h-[48px] bg-[#4F46E5] rounded-full flex items-center justify-center">
             <BsClockFill className="w-[14px] h-[14px] text-white" />
           </div>
 
@@ -35,7 +35,7 @@ const AttendanceSummary = () => {
               {value}
             </div>
           </div>
-        </div>
+        </Card>
       ))}
     </div>
   );
